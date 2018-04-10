3590aefd2dfdeccdab96f1d63d66c38733c462fd
manupa14 <manupa4@live.com.ar>
2018-04-10 18:34:35 -0300

    version inicial

---
+++ src/died/guias/guia02/app/Hola2.java
@@ -0,0 +1,18 @@
+package died.guias.guia02.app;
+
+/**
+ * @author Manu
+ *
+ */
+public class Hola2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("el tiempo actual en milis es "+
+	System.currentTimeMillis());
+		
+	}
+
+}
